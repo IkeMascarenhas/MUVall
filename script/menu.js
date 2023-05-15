e20b2dba8c77ae7ed9bb44e347b4c284c3362e8d
IkeMascarenhas <oikemascarenhas@gmail.com>
2023-05-15 15:18:58 -0300

    atualizacao-imagens-e-Pagina-Perfil

--- script/menu.js
+++ script/menu.js
@@ -1,8 +1,18 @@
+//menu Hamburguer
+
 let menu = document.querySelector('#menu-container')
 let menuItems = document.querySelector('#menu-items')
-
-
-
 menu.addEventListener("click", ()=>{
     menuItems.classList.toggle("show-items")
+})
+
+//Ver mais(perfil)
+
+let adHidden = [...document.querySelectorAll('.hidden')]
+let showAd = document.querySelector('#seeAds')
+
+showAd.addEventListener('click', ()=>{
+    adHidden.map((ad)=>{
+        ad.classList.toggle('show-ads')
+    })
 })
