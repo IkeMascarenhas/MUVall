972b0c3319231ec8e780653f1349603a156ce1b7
IkeMascarenhas <oikemascarenhas@gmail.com>
2023-05-16 16:18:02 -0300

    atualizacao-perfil

--- script/menu.js
+++ script/menu.js
@@ -12,6 +12,7 @@ let adHidden = [...document.querySelectorAll('.hidden')]
 let showAd = document.querySelector('#seeAds')
 
 showAd.addEventListener('click', ()=>{
+    showAd.innerHTML='Ver menos'
     adHidden.map((ad)=>{
         ad.classList.toggle('show-ads')
     })
