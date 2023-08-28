390e627121b2cce397536f5be22e30654b61eb8e
FeLiPeOOF <117963436+FeLiPeOOF@users.noreply.github.com>
2023-08-28 09:14:10 -0300

    Update muvall-bd.sql

--- config/muvall-bd.sql
+++ config/muvall-bd.sql
@@ -3,14 +3,27 @@ CREATE DATABASE MUVALL
 USE MUVALL
 
 CREATE TABLE cadastro (
-  id INT(11) NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   nome VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL,
-  senha VARCHAR(100000) NOT NULL,
+  senha VARCHAR(1000) NOT NULL,
   dataNasc DATE not null,
   PRIMARY KEY (id)
 );
 
+CREATE TABLE anuncio (
+  id INT not null auto_increment,
+  nome_anuncio varchar(50) not null,
+  descricao varchar(200),
+  preco float not null,
+  veiculo varchar(30) not null
+  placa_veiculo varchar(7) not null,
+  capacidade varchar(30) not null,
+  regiao varchar(50) not null,
+  telefone_contato varchar(13) not null,
+  email_contato varchar(100) not null,
+  PRIMARY KEY (id)
+);
 
 
 
