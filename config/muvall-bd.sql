97cd774e0719d90ccb3f8cd1a60b34d7dc55a1d7
FeLiPeOOF <felipe.neress.ss@gmail.com>
2023-10-08 13:30:59 -0300

    bd-final

--- config/muvall-bd.sql
+++ config/muvall-bd.sql
@@ -1,65 +1,40 @@
-CREATE DATABASE MUVALL
+CREATE DATABASE MUVALL;
 
-USE MUVALL
+use MUVALL;
+
+select * from tipo_usuario;
 
 CREATE TABLE usuario (
   id_usuario INT NOT NULL AUTO_INCREMENT,
   nome_usuario VARCHAR(50) NOT NULL,
   email_usuario VARCHAR(50) NOT NULL,
-  senha_usuario VARCHAR(1000) NOT NULL,
-  dataNasc_usuario DATE not null,
-  img_perfil_pasta VARCHAR(80) default NULL,
-  tipo_usuario INT not null default'1',
-  status_usuario INT default'1',
-  PRIMARY KEY (id_usuario)
-);
-
-CREATE TABLE anuncio (
-  id INT not null auto_increment,
-  nome_anuncio varchar(50) not null,
-  descricao varchar(200),
-  preco float not null,
-  veiculo varchar(30) not null,
-  placa_veiculo varchar(7) not null,
-  capacidade varchar(30) not null,
-  regiao varchar(50) not null,
-  telefone_contato varchar(13) not null,
-  email_contato varchar(100) not null,
-  PRIMARY KEY (id)
+  senha_usuario VARCHAR(100000) NOT NULL,
+  dataNasc_usuario varchar(40) not null,
+  status_usuario int DEFAULT '1',
+  PRIMARY KEY (id_usuario),
 );
 
 
+INSERT INTO usuario (nome, email, senha) VALUES
+('Pietro', 'pietro@example.com', 'senha123'),
+('Richard','pistili@example.com', 'senha321'),
+('Felipe', 'neres@example.com', 'senha132');
+
+SELECT * FROM usuario;
 
 
-INSERT INTO cadastro (nome, email, senha) VALUES
-('Pietro', 'pietro@example.com', 'senha2141'),
-('Richard','pistili@example.com', 'senha4321'),
-('Felipe', 'Neres@example.com', 'senh1234');
-
-SELECT * FROM CADASTRO
-
-
-CREATE TABLE cadastro_anunciante (
-  id INT(11) NOT NULL AUTO_INCREMENT,
+CREATE table usuario_anunciante (
+  id_anunciante INT NOT NULL AUTO_INCREMENT,
   nomeDaEmpresa VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  senha VARCHAR(50) NOT NULL,
-  cnpj VARCHAR(14) NOT NULL,
-  fotoPerfil VARCHAR(1000) NOT NULL,
+  email_anunciante VARCHAR(50) NOT NULL,
+  senha_anunciante VARCHAR(50) NOT NULL,
+  cnpj_anunciante VARCHAR(14) NOT NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO cadastro_anunciante (nomeDaEmpresa, email, senha, cnpj, localizacao) 
-VALUES ('Empresa XPTO', 'empresa@exemplo.com', 'senha123', '12345678901234', 'Rua Principal, 123');
+INSERT INTO cadastro_anunciante (nomeDaEmpresa, email, senha, cnpj) 
+VALUES ('Empresa XPTO', 'empresa@exemplo.com', 'senha123', '12345678901234');
 
-SELECT * FROM cadastro_anunciante
+SELECT * FROM usuario_anunciante;
 
-
-
-
-
-
-
-
-
-
+DROP database muvall;
