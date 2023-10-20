80aeb1a401ddf2a414245cb482c6d7639c11bcb8
FeLiPeOOF <felipe.neress.ss@gmail.com>
2023-10-20 02:10:46 -0300

    VOLTEI_TUDO

--- app/routes/router.js
+++ app/routes/router.js
@@ -104,7 +104,7 @@ router.get('/lista-de-servicos', function(req, res){
 
 router.get("/login", function (req, res) {
     res.locals.erroLogin = null
-    res.render("pages/login", { listaErros: null, dadosNotificacao: null});
+    res.render("pages/login", { listaErros: null, dadosNotificacao: null });
   });
   
 router.post(
